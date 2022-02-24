50a2e21e5d7b89ae1f0e92ec74e57ca06ff827b1
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-02-24 08:25:38 -0800

    update test.py

--- tests/test.py
+++ tests/test.py
@@ -136,19 +136,18 @@ class _RuleParse(object):
         return token_set
 
 
+
 rules = [
-    ('object',
-      r'{+\(whitespace+string+whitespace+colon+whitespace+value+comma+newline\)+}'),
+    ('object', r'{+\(whitespace+string+whitespace+colon+whitespace+value+comma+newline\)+}'),
     ('value', r'whitespace+\(string|number|object|array|boolean|null\)+whitespace'),
-    ('comma', r','),
-    ('colon', r':'),
+    ('string', r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"'),
     ('array', r'\[+\(\(whitespace|value\)+comma\)+\]+newline'),
-    ('string',
-      r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"'),
     ('whitespace', r'[ \u0020\u000A\u000D\u0009\t]'),
-    ('newline', r'\n'),
     ('boolean', r'true|false'),
+    ('newline', r'\n'),
     ('NULL', r'null'),
+    ('colon', r':'),
+    ('comma', r','),
     ('mismatch', r'.')
 ]
 
