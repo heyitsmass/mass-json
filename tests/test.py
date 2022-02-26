96a54622843950a1ade1960cae0477a30d499ebf
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-02-25 18:41:07 -0800

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -4,8 +4,8 @@ import re
 from typing import NamedTuple
 
 
-__depth__ = int 
-__filename__ = str 
+__depth__ = int
+__filename__ = str
 
 
 class Token(NamedTuple):
@@ -26,23 +26,19 @@ class _Scanner(object):
     def __init__(self, rules):
         self.rules = _RuleParse(rules)
         self.ids = self.rules.ruleids
-        self.captures = self.rules.captures
 
         for id in self.rules:
             print(id, ':', self.rules[id])
-            if id in self.captures.keys():
-                print('\t', '<capture>:', self.captures[id])
+
 
 
 class _RuleParse(object):
     def __init__(self, rules):
         self._tmp = self._parse(rules)
-        self._data = dict()
+        self._data = self._tmp[0] 
         self.ruleids = self._tmp[1]
-        self.captures = self._tmp[2]
 
-        for x, y in enumerate(self._tmp[1], 0):
-            self._data[y] = self._tmp[0][x]
+
 
     def __iter__(self):
         return iter(self._data)
@@ -62,7 +58,7 @@ class _RuleParse(object):
     def _parse(self, rules):
         rule_ids = [rules[i][0] for i in range(len(rules))]
         captures = dict()
-        tmp = list()
+        tmp = dict()
 
         for rule in rules:
             capture = None          # captured statements
@@ -83,12 +79,12 @@ class _RuleParse(object):
                 i += 1
 
             if capture:
-                tmp.append(self._tokenizer(
-                    (id, condition), rule_ids, capture[0]))
+                tmp[id] = self._tokenizer(
+                    (id, condition), rule_ids, capture[0])
                 continue
-            tmp.append(self._tokenizer((id, condition), rule_ids))
+            tmp[id] = self._tokenizer((id, condition), rule_ids)
 
-        return (tmp, rule_ids, captures)
+        return (tmp, rule_ids)
 
     def _capture(self, i, rule, id, _final=r''):
         _final += '\('
@@ -110,11 +106,14 @@ class _RuleParse(object):
             i += 1
         raise CaptureError(rule)
 
-    def _tokenizer(self, rule, ids, capture=None):
+    def _tokenizer(self, rule, ids, capture=None):                      # Need to fix for tuples 
+        # id = rule[0] 
+        # condition = rule[1] 
+
         statements = [(rule[0], rule[1])] if not capture else [
             (rule[0], st) for st in rule[1].split('+')]
 
-        token_set = list()
+        token_set = tuple()
 
         if __depth__ == 1:
             print("\u001b[1mList of split tokens:\u001b[0m", statements)
@@ -122,9 +121,25 @@ class _RuleParse(object):
 
         for pair in statements:
             if pair[1] in ['<capture>', capture] or pair[1] in ids:
-                token_set.append(pair[1])
+                if pair[1] in ['<capture>']: 
+                    #print(capture) 
+                    tmp = capture.replace('\(', '').replace('\)', '') 
+                    tmp = re.split(r'([|+])', tmp) 
+                    #print(tmp) 
+                    _set = list()
+                    i = 0
+                    while i < len(tmp): 
+                        if i+1 < len(tmp): 
+                            sub_capture = (tmp[i], tmp[i+1])
+                        else: 
+                            sub_capture = tmp[i] 
+                        _set.append(sub_capture)  
+                        i+=2 
+                    token_set + tuple((_set))
+                    continue 
+                token_set + tuple(pair[1])
                 continue
-            token_set.append('(?P<%s>%s)' % pair)
+            token_set + tuple('(?P<%s>%s)' % pair)
 
         if __depth__ > 0:
             print(
@@ -136,12 +151,15 @@ class _RuleParse(object):
         return token_set
 
 
-
 rules = [
-    ('object', r'{+\(whitespace+string+whitespace+colon+whitespace+value+comma+newline\)+}'),
-    ('value', r'whitespace+\(string|number|object|array|boolean|null\)+whitespace'),
-    ('string', r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"'),
-    ('array', r'\[+\(\(whitespace|value\)+comma\)+\]+newline'),
+    ('object',
+        r'{+\(whitespace+string+whitespace+colon+whitespace+value+comma+newline\)+}'),
+    ('value',
+        r'whitespace+\(string|number|object|array|boolean|null\)+whitespace'),
+    ('string',
+        r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"'),
+    ('array',
+        r'\[+\(whitespace|value+comma\)+\]+newline'),
     ('whitespace', r'[ \u0020\u000A\u000D\u0009\t]'),
     ('boolean', r'true|false'),
     ('newline', r'\n'),
@@ -151,13 +169,14 @@ rules = [
     ('mismatch', r'.')
 ]
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     args = argparse.ArgumentParser(
         description="Scans a .json file for grammatical accuracy")
     args.add_argument('filename', metavar='filename',
-                    type=str, help="set the name for the input file")
+                      type=str, help="set the name for the input file")
     args.add_argument('--debug', metavar='N',
-                    type=int, help="set the debug depth level N (default: 0)")
+                      type=int, help="set the debug depth level N (default: 0)",
+                      default=0)
 
     args = vars(args.parse_args())
 
