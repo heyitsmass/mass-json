965ee756d766f937e843593c3e5c09bc6df431c6
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-03-16 16:46:03 -0700

    Updates

--- tests/lexer_new.py
+++ tests/lexer_new.py
@@ -6,6 +6,11 @@ import re
 
   <expression_a> + 
     => <expression_a> is required 
+  
+  <expression_a> ? 
+    => <expression_a> is not necessary 
+
+
 
   
 
@@ -46,9 +51,7 @@ class Rules(object):
               args[i] = tuple(args[i]) 
             
           elif arg[:-1] not in ids: 
-            #self.rules[id] = '(?P<%s>%s)%s' % (id, arg[:-1], arg[-1])
             args[i] = '(?P<%s>%s)%s' % (id, arg[:-1], arg[-1])
-            #print(arg) 
         
         self.rules[id] = tuple(args) 
       else: 
@@ -73,11 +76,113 @@ class Rules(object):
     return self.rules.items() 
 
 
+
+class Scanner(object): 
+  def __init__(self, data, rules): 
+    self.rules = Rules(rules) 
+    self.data = data 
+    self.ids = [id for id in self.rules]
+
+    self.default = self.rules[self.ids[0]][:-1]
+    self.delim = self.rules[self.ids[0]][-1] 
+
+
+    self.__scan(self.default, self.delim) 
+
+  def __scan(self, input, _delim=None): 
+    
+    or_var = None 
+    match = None 
+
+    i = 0
+    while i < len(input): 
+      rule = input[i][:-1]
+      delim = input[i][-1] 
+
+      #print(rule, delim) 
+
+      if rule in self.ids: 
+        tok_regex = self.rules[rule]
+        if type(tok_regex) == tuple: 
+          match = self.__scan(tok_regex, delim)
+          if match: 
+            if i+1 < len(input): 
+              i += 1 
+            else: 
+              i = 0
+            continue 
+          #print(input[i+1]) 
+          raise Exception(tok_regex, match, input, delim) 
+      else: 
+        if '?P' in rule: 
+          tok_regex = rule 
+        elif type(rule) == tuple: 
+          match = self.__scan(rule, delim)
+
+          if match and delim == '?': 
+
+            #print(delim) 
+
+            return match 
+          
+          
+
+          raise Exception(input[i], rule, delim, match) 
+
+      
+
+      match = re.match(tok_regex, self.data) 
+
+    
+      if match: 
+        print(match) 
+
+        self.data = re.sub(tok_regex, '', self.data, 1) 
+
+        if delim == '|': 
+          return match 
+        
+      else: 
+        if delim == '+': 
+          raise Exception(tok_regex, delim) 
+        elif delim == '?': 
+          i+=1
+          continue 
+        elif delim == '|':
+          if i+1 >= len(input): 
+            raise Exception("Unable to find or variable")
+          else: 
+            i+=1
+            continue  
+
+        raise Exception(input, rule, delim, i) 
+      
+      if match and i+1 >= len(input):
+        i = 0
+        continue 
+
+      i+=1 
+      #print(rule, i) 
+
+    return 
+
+
+
+
 if __name__ == '__main__': 
   rule_set = [ 
     ('json', r':=object|,array|=:+'),
-    ('object', r'\{+:=whitespace?,string+,whitespace?,colon+,whitespace?,value+,whitespace?,comma?,whitespace?=:+\}+'),
-    ('whitespace', r'[ \u0020\u000A\u000D\u0009\t]+')
+    ('object', r'\{+:=whitespace?,string+,whitespace?,colon+,whitespace?,value+,whitespace?,comma?,whitespace?=:?\}+'),
+    ('value', r'whitespace?:=string|,number|,object|,array|,boolean|,_null|=:?whitespace?'),
+    ('string', r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"'),
+    ('array', r'\[+:=whitespace?,value+,comma?,whitespace?=:?\]+'),
+    ('number', r'[-]?\d+(?:[.]?\d+)?(?:[Ee]?[-+]?\d+)?'),
+    ('whitespace', r'[ \u0020\u000A\u000D\u0009\t]+'),
+    ('boolean', r'true|false'),
+    ('_null', r'null'),
+    ('colon', r':'),
+    ('comma', r','),
+    ('mismatch', r'.')
   ]
 
-  rules = Rules(rule_set) 
+  scanner = Scanner(open('sample.json', 'r').read(), rule_set) 
