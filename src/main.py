cf0eab79d8f3ee51752e2911d8e584254ebb6e72
♥ Mass ♥ <58617118+heyitsmass@users.noreply.github.com>
2022-04-26 17:15:36 -0700

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+from ruleParser import Rule_parser
+
+raw_rules= [ 
+  ('json', r'(:object|array:)+'), 
+  ('object', r'\{!(:pair!:)*\}!'), 
+  ('value', r'(:string|number|object|array|boolean|Null?:)?'), 
+  ('array', r'\[!(:whitespace?value!comma$whitespace?:)*\]!'),
+  ('pair', r'(:string!whitespace?colon!whitespace?value!whitespace?comma$whitespace?:)?'),
+  ('string', r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"'),
+  ('number', r'[-]?\d+(?:[.]?\d+)?(?:[Ee]?[-+]?\d+)?'),
+  ('whitespace', r'[ \u0020\u000A\u000D\u0009\t]+'),
+  ('boolean', r'true|false'),
+  ('Null', r'null'),
+  ('colon', r':'),
+  ('comma', r','),
+  ('mismatch', r'.')  
+]
+
+parsed_rules = Rule_parser(raw_rules) 
