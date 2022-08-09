398ac5e0a3bd85118e2779d7cf21b984a78b18c3
Brandon <58617118+heyitsmass@users.noreply.github.com>
2022-08-09 01:48:53 -0700

    Update _json_.py
    Added capture group to string expression, simplified value checking, renamed functions

--- tests/_json_.py
+++ tests/_json_.py
@@ -4,21 +4,21 @@ from typing import Any
 
 
 @dataclass 
-class Token: 
+class Value: 
   kind:str 
-  value:Any
+  raw_value:Any
   flag:bool=False
 
 @dataclass
 class Pair: 
   key:str 
-  token:Token
+  value:Value
 
 class _json_: 
   def __init__(self, text): 
     self.text, self.size = text, len(text) 
     self.tokens = { 
-      'STRING' : r'\"(?:(?:(?!\\)[^\"])*(?:\\[/bfnrt\"]|\\u[0-9a-fA-F]{4}|\\\\)?)+?\"', 
+      'STRING' : r'\"((?:(?:(?!\\)[^\"])*(?:\\[/bfnrt\"]|\\u[0-9a-fA-F]{4}|\\\\)?)+?)\"', 
       'NUMBER' : r'[-]?\d+(?:[.]?\d+)?(?:[Ee]?[-+]?\d+)?', 
       'BOOLEAN' : r'true|false', 'NULL' : r'null', 
       'WS' : r'\s*', 'NEWLINE' : r'\n', 'COLON' : r':', 'COMMA' : r',', 
@@ -27,171 +27,172 @@ class _json_:
     } 
     self.line_num = 0
 
-    self.data = self.scan() 
-    
-  def scan(self): 
-    data, index = self.__scanObj() 
+    data, index = self.object() 
 
     if not data: 
-      data, index = self.__scanArr() 
+      data, index = self.array()  
 
-    return data 
+    if index != self.size: 
+      raise RuntimeError("EOF expected") 
 
-  
-  def __scanObj(self, index=0): 
-    ret = dict() 
+  def object(self, index=0): 
+    ret = dict()   
     start = re.match(self.tokens['WS']+self.tokens['DICT_START']+self.tokens['WS'], self.text[index:self.size])
+
+    if not start: 
+      return None, index 
+
+    index += start.span()[1] 
+    flag = True 
+    count = 0
     pair = None 
-    if start: 
+    while index < self.size: 
+      pair, index = self.pair(index)  
 
-      index += start.span()[1] 
-      flag = True 
-      count = line_num = 0
-      while True: 
-        pair, index = self.__getPair(index) 
+      if not pair: 
+        break 
 
-        if not pair: 
-          break    
+      if pair.value and not flag: 
+        raise RuntimeError("Expected ',' on line") 
 
-        if pair.token and not flag: # or pair.token in [dict(), list(), '', 0] and not flag: 
-          raise RuntimeError(f"Expected ',' on line {line_num}") 
+      #print(pair.key, pair.value.raw_value)
 
-        ret[pair.key] = pair.token.value
+      ret[pair.key] = pair.value.raw_value 
 
-        space = re.match(self.tokens['WS'], self.text[index:self.size]) 
-        if space: 
-          index += space.span()[1]  
-        flag = pair.token.flag
-        count+=1 
-      
-      if flag and not pair and count > 0: 
-        raise RuntimeError(f"Unexpected ',' on line {line_num}") 
-      
-      end = re.match(self.tokens['WS'] + self.tokens['DICT_END']+self.tokens['WS'], self.text[index:self.size])  
+      flag = pair.value.flag 
 
-      if end: 
-        index += end.span()[1]  
-        return ret, index
+      count += 1 
 
-    return None, index 
+    if flag and not pair and count > 0: 
+      raise RuntimeError("Unexpected ',' on line")  
 
-  def __scanArr(self, index=0): 
+    end = re.match(self.tokens['WS']+self.tokens['DICT_END']+self.tokens['WS'], self.text[index:self.size])
+
+    if not end: 
+      raise RuntimeError("Expected '}' on line") 
+
+    return ret, index+end.span()[1] 
+
+  def array(self, index=0): 
     ret = list() 
     start = re.match(self.tokens['WS']+self.tokens['ARR_START']+self.tokens['WS'], self.text[index:self.size])
-  
-    if start: 
-      index += start.span()[1] 
-      flag = True 
-      count = line_num = 0
 
-      while True: 
-        token, index = self.__getValue(index)  
+    if not start: 
+      return None, index 
 
-        if not token: 
-          break  
-          
-        if token.kind and not flag: 
-          raise RuntimeError(f"Expected ',' on line on {line_num}") 
+    index += start.span()[1] 
+    val = None 
+    count = 0 
+    flag = True 
+    while index < self.size: 
+      val, index = self.value(index)  
+        
+      if not val: 
+        break  
 
-        ret.append(token.value)  
-
-        flag = token.flag 
-
-        space = re.match(self.tokens['WS'], self.text[index:self.size])  
-
-        index += space.span()[1] if space else 0  
-
-        count += 1 
-
-      if flag and not token and count > 0: 
-        raise RuntimeError(f"Unexpected ',' on line {line_num}")
-
-      end = re.match(self.tokens['WS']+self.tokens['ARR_END']+self.tokens['WS'], self.text[index:self.size]) 
-
-      if end: 
-        index += end.span()[1]
-        return ret, index  
-
-    return None, index 
-
-  
-  def __getValue(self, index=0): 
-    tok_regex = self.tokens['WS']+self.tokens['COMMA'] 
-    for kind in ['STRING', 'NUMBER', 'BOOLEAN', 'NULL', self.__scanObj, self.__scanArr]: 
-      if not callable(kind):  
-        value = re.match(self.tokens[kind], self.text[index:self.size]) 
-        if value: 
-          index += value.span()[1]  
-          comma = re.match(tok_regex, self.text[index:self.size]) 
-          if comma: 
-            index += comma.span()[1]  
-          return Token(kind, self.__convert(value.group(), kind), True if comma else False), index 
-      else: 
-        value, index = kind(index)  
-        if value or value in [dict(), list()]: 
-          comma = re.match(tok_regex, self.text[index:self.size]) 
-          if comma: 
-            index += comma.span()[1]  
-          return Token(kind, value, True if comma else False), index 
-    return None, index 
-
-  def __getPair(self, index=0): 
-    key, index = self.__getKey(index)
-    line_num = 0 
-    if key: 
-      colon = re.match(self.tokens['COLON']+self.tokens['WS'], self.text[index:self.size])  
-
-      if colon: 
-        index += colon.span()[1]
-      else: 
-        raise RuntimeError(f"Missing ':' on line {line_num}") 
-
-      token, index = self.__getValue(index)   
-
-      if not token: 
-        raise RuntimeError(f'Missing value on line {line_num}')  
+      if val.raw_value and not flag: 
+        raise RuntimeError(f"Expected ',' on line")  
       
-      #print(Pair(key, token)) 
+      ret.append(val.raw_value)  
 
-      return Pair(key, token), index  
+      #print(val, index) 
     
-    return None, index 
+      flag = val.flag 
+      count += 1 
 
-  def __getKey(self, index=0):
-    key = re.match(self.tokens['STRING']+self.tokens['WS'], self.text[index:self.size]) 
+    if flag and not val and count > 0: 
+      raise RuntimeError("Unexpected ',' on line")
 
-    if key: 
-      if '\n' in key.group(): 
-        self.line_num += 1 
+    end = re.match(self.tokens['WS']+self.tokens['ARR_END']+self.tokens['WS'], self.text[index:self.size])
 
-      index += key.span()[1] 
-      return self.__convert(key.group(), 'STRING'), index  
+    if not end: 
+      raise RuntimeError("Expected ']' on line")   
     
-    return None, index 
+    return ret, index + end.span()[1] 
 
+  def value(self, index=0, stmts=['STRING', 'NUMBER', 'BOOLEAN', 'NULL']):
+    flag, kind = False, None 
+    
+    tok_regex = '|'.join('(?P<%s>%s)' % (key, self.tokens[key]) for key in stmts)
+    match = re.match(tok_regex, self.text[index:self.size]) 
+
+    if match: 
+      kind = match.lastgroup
+      value = self.convert(match.group(), kind) if kind != 'STRING' else match.groups()[1]
+
+      index += match.span()[1] 
+
+    else: 
+      for func in [self.object, self.array]: 
+        value, index = func(index)  
+        if value: 
+          kind = type(value) 
+          break 
+          
+    if not kind: 
+      return None, index 
+      raise RuntimeError("Missing value on line")  
+
+    cma_regex = self.tokens['WS']+self.tokens['COMMA']+self.tokens['WS']
+    comma = re.match(cma_regex, self.text[index:self.size])  
+    if comma: 
+      index += comma.span()[1] 
+      flag = True 
+
+    return Value(kind, value, flag), index 
+    
+  def key(self, index=0): 
+    
+    tok_regex = r'\"((?:(?:(?!\\)[^\"])*(?:\\[/bfnrt\"]|\\u[0-9a-fA-F]{4}|\\\\)?)+?)\"(\s*)'
+
+    key = re.match(tok_regex, self.text[index:self.size]) 
+    
+    if not key: 
+      return None, index 
+
+    return key.groups()[0]+key.groups()[1], index + key.span()[1] 
   
-  def __convert(self, value:str, kind:str) -> Any: 
-    line_num = 0 
+  def pair(self, index=0): 
+    k, index = self.key(index) 
+
+    col_regex = self.tokens['COLON'] + self.tokens['WS']
+
+    colon = re.match(col_regex, self.text[index:self.size])  
+    
+    if not colon: 
+      return None, index 
+      raise RuntimeError("Expected ':' on line") 
+
+    index += colon.span()[1]
+
+    t, index = self.value(index)  
+
+    if k and t: 
+      return Pair(k, t), index
+
+    return None, index 
+
+  def convert(self, value, kind): 
     match kind: 
-      case 'STRING': 
-        l, r = value.find('\"'), value.rindex('\"')
-        return value[:l] + '' + value[l+1:r] + '' + value[r+1:]
-      case 'BOOLEAN': 
-        return bool(value) 
       case 'NUMBER': 
         if '.' in value: 
           return float(value) 
         return int(value) 
+      case 'BOOLEAN': 
+        return bool(value) 
       case 'NULL': 
-        return None 
-      case _: 
-        raise RuntimeError(f'Unexpected type {kind} on line {line_num}') 
-
-
-  
+        return None  
+      case '_': 
+        raise RuntimeError(f'Unexpected type {kind}') 
 
 
 def load(text:str) -> _json_: 
   return _json_(text) 
+
+
+__filename__ = 'sample.json' 
+
+data = load(open(__filename__, 'r').read()) 
+
 
  
